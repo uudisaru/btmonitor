283e994b8ccb1635354925636cab2fffa3ea592b
Toomas Uudisaru <toomas.uudisaru@aktors.ee>
2019-07-22 15:51:51 +0300

    Pass verification key to Docker

--- docker/start.sh
+++ docker/start.sh
@@ -5,10 +5,20 @@ if [[ ! -f "./servername.txt" ]]; then
     exit 1
 else
     server=$(cat "./servername.txt")
+    verification_key=$(cat "./servername.txt")
+fi
+
+if [[ ! -f "./btmonitor.pub" ]]; then
+    echo "API access verification public key must be defined in file btmonitor.pub!"
+    exit 1
+else
+    server=$(cat "./servername.txt")
+    verification_key=$(cat "./btmonitor.pub")
 fi
 
 docker run -d -p 443:443 \
     -v "$(pwd)/letsencrypt/etc:/etc/letsencrypt" \
     -e "server=${server}" \
+    -e "SANIC_PUBLIC_KEY=\"${verification_key}\"" \
     --restart unless-stopped \
     btmonitor/btmonitor:v1
